a4944abb78f899d66e50cadb640214cb42b303f0
Ehbraheem <ehbraheem@gmail.com>
2017-04-13 11:08:59 +0100

    Content Provider: Content Provider Delete

--- S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
+++ S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -304,11 +304,35 @@ public class WeatherProvider extends ContentProvider {
      */
     @Override
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
-        throw new RuntimeException("Student, you need to implement the delete method!");
+
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+        int numDeleted = 0;
+
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+
+            case CODE_WEATHER:
+
+                numDeleted = db.delete(WeatherContract.WeatherEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+
+            default:
+
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (numDeleted > 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return numDeleted;
 
 //          TODO (2) Only implement the functionality, given the proper URI, to delete ALL rows in the weather table
 
 //      TODO (3) Return the number of rows deleted
+
     }
 
     /**
